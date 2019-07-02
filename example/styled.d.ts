665ccb746dab8bf0a3953803538ca812e75a1d12
Tim de Wolf <tpdewolf@gmail.com>
2019-07-02 23:00:08 +0200

    add grid types

--- example/styled.d.ts
+++ example/styled.d.ts
@@ -1,20 +1,11 @@
 // import original module declarations
-import 'styled-components';
+import 'styled-components'
 
-export interface ThemeGridContainer {
-  maxWidth: number;
-  padding: number | any[];
-}
-
-export interface ThemeGrid {
-  gutter: number | any[];
-  container: ThemeGridContainer;
-}
-
+import { ThemeGrid } from '../src'
 // and extend them!
 declare module 'styled-components' {
   export interface DefaultTheme {
-    breakpoints: string[];
-    grid: ThemeGrid;
+    breakpoints: string[]
+    grid: ThemeGrid
   }
 }
