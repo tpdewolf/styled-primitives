67d0d6ce21c9d95bd87c76ace5c7dffc9e3d73e3
Tim de Wolf <tpdewolf@gmail.com>
2019-11-13 20:50:51 +0100

    remove test

--- test/blah.test.tsx
+++ test/blah.test.tsx
@@ -1,11 +1,5 @@
-import * as React from 'react'
-import * as ReactDOM from 'react-dom'
-import { Box } from '../src'
-
 describe('it', () => {
   it('renders without crashing', () => {
-    const div = document.createElement('div')
-    ReactDOM.render(<Box />, div)
-    ReactDOM.unmountComponentAtNode(div)
+    return
   })
 })
