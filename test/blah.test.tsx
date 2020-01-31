e49e03817b84a24cd2b1ae6672485a33b9e09ae2
Robby Uitbeijerse <robbyuitbeijerse@gmail.com>
2020-01-31 13:19:41 +0100

    move the js-dom env as its set by default, move the tsconfig file as the tests were not reading it, add a test for Box that uses the tsconfig file and update depedencies

--- test/blah.test.tsx
+++ test/blah.test.tsx
@@ -1,5 +1,11 @@
-describe('it', () => {
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+import { Box } from '../src/Box'
+
+describe('Box', () => {
   it('renders without crashing', () => {
-    return
+    const div = document.createElement('div')
+    ReactDOM.render(<Box />, div)
+    ReactDOM.unmountComponentAtNode(div)
   })
 })
