035bb4ce6f951e526ea1b39fe04d11144b4785eb
Tim de Wolf <tpdewolf@gmail.com>
2020-02-23 18:07:51 +0100

    update dependencies and add BoxProps to Card

--- src/Card.tsx
+++ src/Card.tsx
@@ -7,8 +7,8 @@ import {
   compose,
 } from 'styled-system'
 
-import { Box } from './Box'
+import { Box, BoxProps } from './Box'
 
-export type CardProps = BorderProps & ShadowProps
+export type CardProps = BoxProps & BorderProps & ShadowProps
 
 export const Card = styled(Box)<CardProps>(compose(border, shadow))
